dafdc222c5d4c181a85d23c94791aa9c8a99bd77
ihn <ch200203@hamil.com>
2022-04-25 16:50:35 +0900

    Spring Jwt Login 예시 업로드

---
+++ jwt-login/src/main/java/com/example/restlogin/JwtLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.restlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtLoginApplication.class, args);
+	}
+
+}
---
+++ jwt-login/src/test/java/com/example/restlogin/JwtLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
