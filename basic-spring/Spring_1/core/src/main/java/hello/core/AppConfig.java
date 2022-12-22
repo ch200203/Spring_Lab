d9d7db58db4797147a44330d01d0188bd45c28b0
ch200203 <ch200205@naver.com>
2022-12-22 16:35:20 +0900

    fix : Spring 컨테이너 등록

--- basic-spring/Spring_1/core/src/main/java/hello/core/AppConfig.java
+++ basic-spring/Spring_1/core/src/main/java/hello/core/AppConfig.java
@@ -8,28 +8,35 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
-        public MemberService memberService() {
-            // 생성자 주입
-            return new MemberServiceImpl(new MemoryMemberRepository());
-        }
+    @Bean
+    public MemberService memberService() {
+        // 생성자 주입
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
 
-        private MemoryMemberRepository memberRepository() {
-            return new MemoryMemberRepository();
-        }
+    @Bean
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
-        public OrderService orderService() {
-            return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                // new FixDiscountPolicy()
-                new RateDiscountPolicy()
-            );
-        }
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(
+            new MemoryMemberRepository(),
+            // new FixDiscountPolicy()
+            new RateDiscountPolicy()
+        );
+    }
 
-        public DiscountPolicy discountPolicy() {
-            return new FixDiscountPolicy();
-        }
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 
 }
--- basic-spring/Spring_1/core/src/main/java/hello/core/MemberApp.java
+++ basic-spring/Spring_1/core/src/main/java/hello/core/MemberApp.java
@@ -4,14 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
         // MemberService 호출 => 구현체 MemberServiceImpl 선택
         // MemberService memberService = new MemberServiceImpl();
+
+        // Spring 컨테이너에 등록
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         // Member 객체 생성 => (Ctrl + ALT + V 단축키 사용)
         Member member = new Member(1L, "memberA", Grade.VIP);
