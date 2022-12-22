90faf63d697d4df8c1cdad58b3f48ba0d1150d89
ch200203 <ch200205@naver.com>
2022-12-22 14:43:00 +0900

    fix : 코드 정리

--- basic-spring/Spring_1/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ basic-spring/Spring_1/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,11 +1,8 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
