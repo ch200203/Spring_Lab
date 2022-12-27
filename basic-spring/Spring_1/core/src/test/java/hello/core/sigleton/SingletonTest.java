df5b40beac0e8d604a8968c4ab86a3bfd13ad4cf
ch200203 <ch200205@naver.com>
2022-12-27 22:51:40 +0900

    feat : 싱글톤 객체 테스트 코드 작성, 스프링컨테이너 싱글톤 테스트 코드 작성

--- basic-spring/Spring_1/core/src/test/java/hello/core/sigleton/SingletonTest.java
+++ basic-spring/Spring_1/core/src/test/java/hello/core/sigleton/SingletonTest.java
@@ -2,9 +2,12 @@ package hello.core.sigleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
+import hello.core.singleton.SingletonService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -26,6 +29,43 @@ public class SingletonTest {
 
 
         assertThat(memberService1).isNotSameAs(memberService2);
-
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    public void singletonServiceTest() {
+
+        //private 으로 생성자를 막아두었다. 컴파일 오류가 발생한다.
+        // new SingletonService();
+
+        // 1. 조회 : 호출 할 떄 마다 같은 객체를 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+
+        // 2. 조회 : 호출 할 떄 마다 같은 객체를 반환
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        // 참조값이 같은 것을 확인
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        // same == 인스턴스 비교
+        // singletonService1 == singletonService2
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        // AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
+    }
+
 }
