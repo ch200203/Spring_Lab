2f7df2a96e50f1341c889fca75b07dbc7e1fcf54
ch200203 <ch200205@naver.com>
2022-12-28 11:21:17 +0900

    fix : 싱글톤 호출로그 생성

--- basic-spring/Spring_1/core/src/main/java/hello/core/AppConfig.java
+++ basic-spring/Spring_1/core/src/main/java/hello/core/AppConfig.java
@@ -17,20 +17,28 @@ public class AppConfig {
     // @Bean memberService -> new MemoryMemberRepository()
     // @Bean orderService -> new MemoryMemberRepository();
 
+    // Spring의 싱글톤 보장
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+
     // @Bean(name = "other_name")
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         // 생성자 주입
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
             memberRepository(),
             // new FixDiscountPolicy()
