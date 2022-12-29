c790932da0430ff5116043097ab7c4419fefa09e
ch200203 <ch200205@naver.com>
2022-12-29 15:43:10 +0900

    fix : 컴포넌트 스캔 어노테이션 추가

--- basic-spring/Spring_1/core/src/main/java/hello/core/AutoAppConfig.java
+++ basic-spring/Spring_1/core/src/main/java/hello/core/AutoAppConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(
     // ComponentScan에서 제외할 목록 (필터)를 지정
     // 단, 실무에서는 이렇게 지정하지는 않음, 예제코드를 사용하기 위해서 필터를 지정함
+    // basePackages = "hello.core.member",
+    // basePackageClasses = AutoAppConfig.class,
     excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
--- basic-spring/Spring_1/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ basic-spring/Spring_1/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -3,7 +3,6 @@ package hello.core.scan;
 import hello.core.AutoAppConfig;
 import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
