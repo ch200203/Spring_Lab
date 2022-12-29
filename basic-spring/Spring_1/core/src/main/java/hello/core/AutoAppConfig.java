c2714cd19e03557588fdaeefd4a9c8cd5e391bc3
ch200203 <ch200205@naver.com>
2022-12-29 17:24:31 +0900

    fix : 컴포넌트 스캔, 빈 이름 중복 주석

--- basic-spring/Spring_1/core/src/main/java/hello/core/AutoAppConfig.java
+++ basic-spring/Spring_1/core/src/main/java/hello/core/AutoAppConfig.java
@@ -18,8 +18,8 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
+/*    @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository() {
         return new MemoryMemberRepository();
-    }
+    }*/
 }
