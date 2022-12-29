7b6b9a70f012bd7b11facd461af1e6dfb1a66b2b
ch200203 <ch200205@naver.com>
2022-12-29 10:12:09 +0900

    fix : @Component 어노테이션 적용

--- basic-spring/Spring_1/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ basic-spring/Spring_1/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,8 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
--- basic-spring/Spring_1/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ basic-spring/Spring_1/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -2,7 +2,9 @@ package hello.core.member;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     // 동시성 이슈가 있을 수 있기 때문에 => 실무에서는 ConcurrnetHashMap을 보통 사용 
