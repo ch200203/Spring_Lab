098f81607f6666536d7646070951c019f9398ab5
ch200203 <ch200205@naver.com>
2023-01-08 18:27:47 +0900

    feat : 빈 생명주기 콜백 @PostConstruct, @PreDestroy 테스트 코드 작성

--- basic-spring/Spring_1/core/src/main/java/hello/core/lifecycle/NetworkClient.java
+++ basic-spring/Spring_1/core/src/main/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,17 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -27,4 +31,18 @@ public class NetworkClient {
     public void disconnect() {
         System.out.println("close: " + url);
     }
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+
+
 }
--- basic-spring/Spring_1/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ basic-spring/Spring_1/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -2,7 +2,7 @@ package hello.core.member;
 
 import java.util.HashMap;
 import java.util.Map;
-import org.springframework.stereotype.Component;
+    import org.springframework.stereotype.Component;
 
 @Component
 public class MemoryMemberRepository implements MemberRepository {
