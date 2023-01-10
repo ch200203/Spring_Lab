52be1c92cafbc686d1d0f95e24a1cb5dec5157fc
ch200203 <ch200205@naver.com>
2023-01-11 00:11:59 +0900

    feat : MyLogger 클래스 생성

---
+++ basic-spring/Spring_1/core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,48 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    /**
+     * requestURL 빈이 생성되는 시점에 알 수 없으므로 외부에서 setter로 입력 받는다.
+     * @param requestURL
+     */
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" +uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    /**
+     * 빈이 생성되는 시점에 @PostConstruct를 사용해서 uuid를 생성하여 저장한다.
+     * HTTP 요청 하나당 하나씩 생성되며, uuid를 저장하면 다른 요청과 구분이 가능하다.
+     */
+    @PostConstruct
+    public void init() {
+
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" +uuid + "]" + "request scope bean create : " + this);
+    }
+
+    /**
+     * 빈요 소멸하는 시점에 @PreDestroy를 사용하여 종료 메시지를 남긴다.
+     */
+    @PreDestroy
+    public void close() {
+        System.out.println("[" +uuid + "]" + "request scope bean close : " + this);
+    }
+
+}
