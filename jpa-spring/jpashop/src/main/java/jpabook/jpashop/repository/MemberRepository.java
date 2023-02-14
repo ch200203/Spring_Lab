b9d0e77dae750dbcd56b0ff512808852f0569149
ch200203 <ch200205@naver.com>
2023-02-14 22:51:23 +0900

    feat : MemberRepository 개발 및 구현

---
+++ jpa-spring/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
+import java.util.List;
+
+@Repository // Spring Bean 으로 등록, 컴포넌트 스캔의 대상
+public class MemberRepository {
+
+    @PersistenceContext // EntityManager 를 스프링이 주입
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        // JPQL에서의 from의 대상은 table이 아니라 Entity가 대상임
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
