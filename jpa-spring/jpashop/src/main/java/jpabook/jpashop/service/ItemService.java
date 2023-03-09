6a6da51f92881fe6964d1c9380e333af98338652
ch200203 <ch200205@naver.com>
2023-03-09 23:05:51 +0900

    feat : add ItemList

--- jpa-spring/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpa-spring/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -20,7 +20,7 @@ public class ItemService {
         itemRepository.save(item);
     }
 
-    public List<Item> findItems(Item item) {
+    public List<Item> findItems() {
         return itemRepository.findAll();
     }
 
