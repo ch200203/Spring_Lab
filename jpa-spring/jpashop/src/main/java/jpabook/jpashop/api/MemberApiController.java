f5c1b3d0b98399464d6856c4ef354f64456d9013
ch200203 <ch200205@naver.com>
2023-07-20 13:46:16 +0900

    feat: member update Method 변경감지

--- jpa-spring/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpa-spring/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -17,15 +17,9 @@ public class MemberApiController {
     private final MemberService memberService;
 
     /**
-     * 등록 V1: 요청 값으로 Member 엔티티를 직접 받는다.
-     * 문제점
-     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
-     * - 엔티티에 API 검증을 위한 로직이 들어간다. (@NotEmpty 등등)
-     * - 실무에서는 회원 엔티티를 위한 API가 다양하게 만들어지는데, 한 엔티티에 각각의 API를 위
-     한 모든 요청 요구사항을 담기는 어렵다.
-     * - 엔티티가 변경되면 API 스펙이 변한다.
-     * 결론
-     * - API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다.
+     * 등록 V1: 요청 값으로 Member 엔티티를 직접 받는다. 문제점 - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다. - 엔티티에 API 검증을 위한 로직이
+     * 들어간다. (@NotEmpty 등등) - 실무에서는 회원 엔티티를 위한 API가 다양하게 만들어지는데, 한 엔티티에 각각의 API를 위 한 모든 요청 요구사항을 담기는
+     * 어렵다. - 엔티티가 변경되면 API 스펙이 변한다. 결론 - API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다.
      */
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody
@@ -53,6 +47,7 @@ public class MemberApiController {
      */
     @Data
     static class CreateMemberRequest {
+
         @NotEmpty
         private String name;
     }
