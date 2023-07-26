49bc46859f34ce95c96c5bf935cdf69a05926e74
ch200203 <ch200205@naver.com>
2023-07-27 00:34:18 +0900

    feat : find All by String

--- jpa-spring/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpa-spring/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -25,22 +25,66 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
+    public List<Order> findAll() {
+        return em.createQuery("select o from Order o", Order.class)
+            .getResultList();
+    }
+
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+            .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+    }
+
     // 문제가 되는 부분
     public List<Order> findALlOrigin(OrderSearch orderSearch) {
         return em.createQuery("select o from Order o join o.member m" +
-                        " where o.status = :status" +
-                        " and m.name like :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
-                // .setFirstResult() -> 시작 값 페이징 처리에 활용
-                .setMaxResults(1000) // 최대 1000건
-                .getResultList();
+                " where o.status = :status" +
+                " and m.name like :name", Order.class)
+            .setParameter("status", orderSearch.getOrderStatus())
+            .setParameter("name", orderSearch.getMemberName())
+            // .setFirstResult() -> 시작 값 페이징 처리에 활용
+            .setMaxResults(1000) // 최대 1000건
+            .getResultList();
     }
 
     /**
-     * JPA Criteria JPA 표준 스펙이지만 실무에서 사용하기에 너무 복잡
-     * JPA 에서 동적 쿼리생성을 위한 기능 -> 실무에서 활용하지 않음
-     * 따라서 이를 해결하기 위해 Querydsl 이 사용되게됨
+     * JPA Criteria JPA 표준 스펙이지만 실무에서 사용하기에 너무 복잡 JPA 에서 동적 쿼리생성을 위한 기능 -> 실무에서 활용하지 않음 따라서 이를 해결하기
+     * 위해 Querydsl 이 사용되게됨
      */
     public List<Order> findAllByCriteria(OrderSearch orderSearch) {
         CriteriaBuilder cb = em.getCriteriaBuilder();
@@ -53,14 +97,14 @@ public class OrderRepository {
         //주문 상태 검색
         if (orderSearch.getOrderStatus() != null) {
             Predicate status = cb.equal(o.get("status"),
-                    orderSearch.getOrderStatus());
+                orderSearch.getOrderStatus());
             criteria.add(status);
         }
 
         //회원 이름 검색
         if (StringUtils.hasText(orderSearch.getMemberName())) {
             Predicate name =
-                    cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+                cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
             criteria.add(name);
         }
 
