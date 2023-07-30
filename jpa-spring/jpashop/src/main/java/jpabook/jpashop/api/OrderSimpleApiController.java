7c7a0c64e34a7edb0d32a0443b54ea41bc370654
ch200203 <ch200205@naver.com>
2023-07-30 23:38:04 +0900

    refactor : create Simple orderV3 fetch join

--- jpa-spring/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpa-spring/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -17,11 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * xToNone 관계의 성능 최적화
- * (ManyToOne, OneToOne)
- * Order
- * Order -> Member
- * Order -> Delivery
+ * xToNone 관계의 성능 최적화 (ManyToOne, OneToOne) Order Order -> Member Order -> Delivery
  */
 @RestController
 @RequiredArgsConstructor
@@ -33,7 +29,7 @@ public class OrderSimpleApiController {
     public List<Order> ordersV1() {
 
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
-        for(Order order : all) {
+        for (Order order : all) {
             order.getMember().getName(); // Lazy 강제 로딩
             order.getDelivery().getAddress(); // Lazy 강제 로딩
         }
@@ -56,21 +52,46 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v2/simple-orders")
     public List<SimpleOrderDto> ordersV2() {
         // 실제로는 List가 아니라 Result로 반환을 해야 함
-
+        // ORDER 2개
+        // N + 1 문제 -> 1 + 회원 N + 배송 N
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
         List<SimpleOrderDto> result = orders.stream()
             .map(o -> new SimpleOrderDto(o))
             .collect(Collectors.toList());
 
         return result;
-        /*return orderRepository.findAllByString(new OrderSearch()).stream()
+        /*
+        return orderRepository.findAllByString(new OrderSearch()).stream()
             .map(SimpleOrderDto::new)
-            .collect(toList());*/
+            .collect(toList());
+        */
+
+        /**
+         * orderV1 과 orderV2의 문제점
+         * 쿼리가 1 + N + N 번 실행된다.
+         * N + 1 문제가 발생함
+         */
     }
+
+    /**
+     *  V3. 엔티티를 조회해서 DTO로 변환 (fetch join 사용)
+     * - fetch join으로 쿼리 1번 호출
+     * * 자세한 내용은 기본편의 fetch 조인을 참고
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.finAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+            .map(o -> new SimpleOrderDto(o))
+            .collect(toList());
+        return result;
+    }
+
 
     @Data
     @AllArgsConstructor
     static class SimpleOrderDto {
+
         private Long orderId;
         private String name;
         private LocalDateTime orderDate;
@@ -79,14 +100,12 @@ public class OrderSimpleApiController {
 
         public SimpleOrderDto(Order order) {
             orderId = order.getId();
-            name = order.getMember().getName();
+            name = order.getMember().getName(); // LAZY 초기화
             orderDate = order.getOrderDate();
             orderStatus = order.getStatus();
-            address = order.getDelivery().getAddress();
-
+            address = order.getDelivery().getAddress(); // LAZY 초기화
         }
     }
-
 
 
 }
