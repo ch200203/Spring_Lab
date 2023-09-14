911a6b6ab9caf7aa533d840169605cce75452899
ch200203 <ch200205@naver.com>
2023-09-14 23:24:50 +0900

    feat : ToOne(N:1, 1:)관계들을 먼저 조회하고, ToMany(1:N) 관계는 각각 별도로 처리하여 최적화하기

--- jpa-spring/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpa-spring/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     /**
      * V1. 엔티티 직접 노출
@@ -104,6 +107,19 @@ public class OrderApiController {
             .collect(Collectors.toList());
 
         return result;
+    }
+기
+    /**
+     * ToOne(N:1, 1:1)관계들을 먼저 조회하고, ToMany(1:N) 관계는 각각 별도로 처리한다
+     *  - 이런 방식을 선택한 이유는
+     *  - ToOne 관계는 조인해도 데이터 row 수가 증가하지 않는다.
+     *  - ToMany 관계는 조인하면 row 수가 증가한다.
+     * row 수가 증가하지 않는 ToOne 관계는 조인으로 최적화하기 쉬우므로 한번에 조회하고,
+     * ToMany 관계는 최적화가 어려우므로 'findOrderItems()'같은 별도의 메서드로 처리한다.
+     */
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
     }
 
 
